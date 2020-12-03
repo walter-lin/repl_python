ce90855d061466906da3e9e7d14c93215a7e42e1
walter-lin <wayne0240306256@gmail.com>
2020-12-03 18:10:55 +0800

    5.2完成

---
+++ repl_5.py
@@ -0,0 +1,23 @@
+#1
+# s = input()
+# print(s[2]) #1
+# print(s[-2]) #2
+# print(s[:5]) #3
+# print(s[:-2]) #4
+# print(s[0::2]) #5
+# print(s[1::2]) #6
+# print(s[::-1]) #7
+# print(s[::-2]) #8
+# print(len(s))
+
+#2
+# s = input()
+# cout = 1
+# for i in s:
+#     if i == ' ':
+#         cout += 1 
+# print(cout)
+
+# #another ans
+# print(len(input().split()))
+
