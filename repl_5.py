e8c18713b6776da3f3746268525b59794a235376
walter-lin <wayne0240306256@gmail.com>
2020-12-04 21:23:34 +0800

    5.5完成

--- repl_5.py
+++ repl_5.py
@@ -1,4 +1,4 @@
-#1
+#5.1
 # s = input()
 # print(s[2]) #1
 # print(s[-2]) #2
@@ -10,7 +10,7 @@
 # print(s[::-2]) #8
 # print(len(s))
 
-#2
+#5.2
 # s = input()
 # cout = 1
 # for i in s:
@@ -18,15 +18,42 @@
 #         cout += 1 
 # print(cout)
 
-# #another ans
+# else
 # print(len(input().split()))
 
-#3
+#5.3
 # s = input()
 # cont = int(len(s)/2)
 # print(s[-cont:] , s[:(-cont)] , sep='' )
 
-# #another ans
+# else
 # s = input()
 # mid = (len(s) + 1) // 2
 # print(s[mid:] + s[:mid])
+
+#5.4
+# s = input().split()
+# print(s[-1] , s[-2])
+
+# else
+# word1, word2 = input().split()
+# print(word2, word1)
+
+#5.5
+# s = input()
+# f = -1
+# l = ''
+# for i in range(0,(len(s))):
+#     if s[i] == 'f'and f == -1:
+#         f = i
+#     elif s[i] == 'f':
+#         l = i
+# print(f , l)
+
+# else
+# s = input()
+# if s.find('f') == s.rfind('f'):
+#   print(s.find('f'))
+# else:
+#   print(s.find('f'), s.rfind('f'))
+
